fe7cd69a5e06473b05f7bfe698050d757da68d37
Nur Kasimov <nur.kas99@ya.ru>
2021-03-14 16:04:01 +0500

    add answer

--- task1.py
+++ task1.py
@@ -9,31 +9,37 @@ def f(x, y):
     return (1-x)**2 + (100*((y-x**2)**2))
 
 # начальн. значение
-xn = -1
-yn = -1
+xn = 0
+yn = 0
 N = f(xn, yn)
 change_x = True;
 change_y = True;
 
-# заводим словарь, где будем хранить все найденные значения функции
-Y = {N: (xn, yn)}
+# словарь со всеми найденными значениями функции
+values = {N: (xn, yn)}
 
 # градиентный спуск
 for iteri in range(max_iterations):
-	if (change_x):
-		xn_new = xn + step*diff((1-x)**2 + (100*(y-x**2)**2), x).subs({x:xn, y:yn})
+    if (change_x):
+        xn_new = xn + step*diff((1-x)**2 + (100*(y-x**2)**2), x).subs({x:xn, y:yn})
     if (change_y):
-	yn_new = yn + step*diff((1-x)**2 + (100*(y-x**2)**2), y).subs({x:xn, y:yn})
+        yn_new = yn + step*diff((1-x)**2 + (100*(y-x**2)**2), y).subs({x:xn, y:yn})
     
-    N = round(f(xn_new, yn_new), 2)
-    Y[N] = (xn_new, yn_new)
-    
-    if ((xn_new < -3 or xn_new > 3) or abs(xn_new - xn) < 0.01):
-        xn = xn_new
+    if ((xn_new < -3 or xn_new > 3)):
         change_x = False
+    else: 
+        xn = xn_new
         
-    if ((yn_new < -3 or yn_new > 3) or abs(yn_new - yn) < 0.01):
-        yn = yn_new
+    if ((yn_new < -3 or yn_new > 3)):
         change_y = False
-		
-	if (not change_x and not change_y): break
+    else:
+        yn = yn_new
+
+    if (not change_x and not change_y): 
+        break
+        
+    N = round(f(xn, yn), 2)
+    values[N] = (xn, yn)
+        
+max_v = max(values.keys())
+print("max:", max_v, values[max_v])
