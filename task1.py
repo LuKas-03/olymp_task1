68ab58c469d82d25e768a5607247e140d8e3f2ad
Nur Kasimov <nur.kas99@ya.ru>
2021-03-14 14:22:59 +0500

    ашч

--- task1.py
+++ task1.py
@@ -1,7 +1,8 @@
 from scipy.misc import derivative
 
-# шаг 
+# шаг и ограничение циклом
 step = 0.0001
+max_iterations = 5000
 
 # определяем функцию
 def f(x, y):
@@ -17,19 +18,22 @@ change_y = True;
 # заводим словарь, где будем хранить все найденные значения функции
 Y = {N: (xn, yn)}
 
-# по формуле градиентного спуска получаем все значения x y
-for iteri in range(5000):
-    xn_new = xn + step*diff((1-x)**2 + (100*(y-x**2)**2), x).subs({x:xn, y:yn})
-    yn_new = yn + step*diff((1-x)**2 + (100*(y-x**2)**2), y).subs({x:xn, y:yn})
+# градиентный спуск
+for iteri in range(max_iterations):
+	if (change_x):
+		xn_new = xn + step*diff((1-x)**2 + (100*(y-x**2)**2), x).subs({x:xn, y:yn})
+    if (change_y):
+	yn_new = yn + step*diff((1-x)**2 + (100*(y-x**2)**2), y).subs({x:xn, y:yn})
     
     N = round(f(xn_new, yn_new), 2)
     Y[N] = (xn_new, yn_new)
-    if_change_x = False;
     
-    if ((xn_new > -3 and xn_new < 3) and abs(xn_new - xn) >= 0.01):
+    if ((xn_new < -3 or xn_new > 3) or abs(xn_new - xn) < 0.01):
         xn = xn_new
-        if_change = True
+        change_x = False
         
-    if ((yn_new > -3 and yn_new < 3) and abs(yn_new - yn) >= 0.01):
+    if ((yn_new < -3 or yn_new > 3) or abs(yn_new - yn) < 0.01):
         yn = yn_new
-        if_change = True
+        change_y = False
+		
+	if (not change_x and not change_y): break
